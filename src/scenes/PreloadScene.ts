6697998fc0065707408366bc24842f6a998e2907
Yauheni Paulau <7647901@gmail.com>
2026-02-18 00:25:06 +0100

    Add new tiles and tilemap for Kenney Monochrome Pirates assets

--- src/scenes/PreloadScene.ts
+++ src/scenes/PreloadScene.ts
@@ -15,45 +15,47 @@ export class PreloadScene extends Phaser.Scene {
     loadingText.setOrigin(0.5);
 
     // Dungeon tile textures from kenney_monochrome-pirates pack
+    // Menu background (preview) used by MenuScene
+    this.load.image('menu-background', '/assets/preview.png');
     this.load.image(
       'dungeon-wall-tile',
-      'assets/kenney_monochrome-pirates/Monochrome/Tiles/tile_0106.png'
+      'assets/kenney_monochrome-pirates/Monochrome/Tiles/tile_0018.png'
     );
     this.load.image(
       'dungeon-floor-tile',
-      'assets/kenney_monochrome-pirates/Monochrome/Tiles/tile_0059.png'
+      'assets/kenney_monochrome-pirates/Monochrome/Tiles/tile_0021.png'
     );
     this.load.image(
       'player-ship-tile',
-      'assets/kenney_monochrome-pirates/Monochrome/Tiles/tile_0001.png'
+      'assets/kenney_monochrome-pirates/Monochrome/Tiles/tile_0124.png'
     );
     this.load.image(
       'player-shovel-tile',
-      'assets/kenney_monochrome-pirates/Monochrome/Tiles/tile_0002.png'
+      'assets/kenney_monochrome-pirates/Monochrome/Tiles/tile_0087.png'
     );
     this.load.image(
       'enemy-weak-tile',
-      'assets/kenney_monochrome-pirates/Monochrome/Tiles/tile_0014.png'
+      'assets/kenney_monochrome-pirates/Monochrome/Tiles/tile_0127.png'
     );
     this.load.image(
       'enemy-fast-tile',
-      'assets/kenney_monochrome-pirates/Monochrome/Tiles/tile_0015.png'
+      'assets/kenney_monochrome-pirates/Monochrome/Tiles/tile_0125.png'
     );
     this.load.image(
       'enemy-tank-tile',
-      'assets/kenney_monochrome-pirates/Monochrome/Tiles/tile_0016.png'
+      'assets/kenney_monochrome-pirates/Monochrome/Tiles/tile_0126.png'
     );
     this.load.image(
       'merchant-tile',
-      'assets/kenney_monochrome-pirates/Monochrome/Tiles/tile_0038.png'
+      'assets/kenney_monochrome-pirates/Monochrome/Tiles/tile_0127.png'
     );
     this.load.image(
       'treasure-locked-tile',
-      'assets/kenney_monochrome-pirates/Monochrome/Tiles/tile_0099.png'
+      'assets/kenney_monochrome-pirates/Monochrome/Tiles/tile_0134.png'
     );
     this.load.image(
       'treasure-unlocked-tile',
-      'assets/kenney_monochrome-pirates/Monochrome/Tiles/tile_0100.png'
+      'assets/kenney_monochrome-pirates/Monochrome/Tiles/tile_0135.png'
     );
     this.load.image(
       'treasure-chest-tile',
